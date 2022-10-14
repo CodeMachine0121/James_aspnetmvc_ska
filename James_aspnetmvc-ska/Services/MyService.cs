8a401bc6761a3013177539b104101c02a3f53bf5
James HSUEH Hua-Cheng <james.hsueh@titansoft.com.sg>
2022-10-14 12:43:33 +0800

    Add static file storing

--- James_aspnetmvc-ska/Services/MyService.cs
+++ James_aspnetmvc-ska/Services/MyService.cs
@@ -2,26 +2,33 @@
 using Newtonsoft.Json;
 
 namespace James_aspnetmvc_ska.Services;
+
 public class MyService
 {
     // TODO: Get/Store Data
-    private List<AccountModel> dataList = new List<AccountModel>();
+    private readonly List<AccountModel>? dataList= new List<AccountModel>();
 
+    public MyService()
+    {
+        if (File.Exists("./dataList.json"))
+            dataList = JsonConvert.DeserializeObject<List<AccountModel>>(File.ReadAllText("./dataList.json"));
+    }
 
-    public List<AccountModel> GetData()
+    public List<AccountModel>? GetData()
     {
         return dataList;
     }
 
-    
     public string GetDataString()
-    { // for json serialize
+    {
+        // for json serialize
         return JsonConvert.SerializeObject(dataList);
     }
-    public List<AccountModel> AddData(AccountModel data)
+
+    public List<AccountModel>? AddData(AccountModel data)
     {
         dataList.Add(data);
+        File.WriteAllText("./dataList.json", GetDataString());
         return GetData();
     }
-    
 }
